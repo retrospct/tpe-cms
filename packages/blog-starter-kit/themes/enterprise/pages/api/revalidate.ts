3682651c34b67b5d7dde5d92d26e6b3a351846d5
Justin Lee <me@jlee.cool>
2024-03-18 12:16:50 -0700

    fix: webhook secret fix

--- packages/blog-starter-kit/themes/enterprise/pages/api/revalidate.ts
+++ packages/blog-starter-kit/themes/enterprise/pages/api/revalidate.ts
@@ -4,7 +4,7 @@ import { validateSignature } from '../../utils/signing'
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     // Check for secret to confirm this is a valid request
-    const validateRequest = validateSignature({incomingSignatureHeader: req.headers['x-hashnode-signature'], payload: req.body, secret: process?.env?.HN_WEBHOOK_SECRET || null})
+    const validateRequest = validateSignature({incomingSignatureHeader: req.headers['x-hashnode-signature'], payload: req.body, secret: process.env?.HN_WEBHOOK_SECRET as string || null})
     const { data } = req.body
     if (!validateRequest.isValid) return res.status(401).json({ message: 'Invalid token' })
     if (!data) return res.status(400).json({ message: 'No data provided' })
